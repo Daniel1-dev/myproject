1707d7785c3dc4d29ddf4f003556d9f853c9c020
YourName <eyohdaniel65@gmail.com>
2026-05-26 18:48:21 +0100

    Fix database config

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,13 +87,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 
+
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        conn_max_age=600
     )
 }
-
-
 
 
 
